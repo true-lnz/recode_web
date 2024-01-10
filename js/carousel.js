e15f8bc1e02223b1a3db11e6346da3c60eade4c0
true-lnz <lansonzgroup@yandex.ru>
2024-01-11 02:03:07 +0500

    horizontal scroll update

---
+++ js/carousel.js
@@ -0,0 +1,37 @@
+const scrollwrapper = document.querySelector('.carousel-container');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+scrollwrapper.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - scrollwrapper.offsetLeft;
+  scrollLeft = scrollwrapper.scrollLeft;
+});
+scrollwrapper.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+scrollwrapper.addEventListener('mouseup', () => {
+  isDown = false;
+});
+scrollwrapper.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - scrollwrapper.offsetLeft;
+  const walk = (x - startX) * 1; //scroll-fast
+  scrollwrapper.scrollLeft = scrollLeft - walk;
+});
+
+const scrollToRight = () => {
+	scrollwrapper.scrollBy({
+		left: 200,
+		behavior: 'smooth'
+	});
+}
+
+const scrollToLeft = () => {
+	scrollwrapper.scrollBy({
+		left: -200,
+		behavior: 'smooth'
+	});
+}
